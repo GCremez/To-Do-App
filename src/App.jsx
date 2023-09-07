0edacf9c65241aa6ce42a68657605ded1eaf548f
GCremez <salauolorunjuedalo@gmail.com>
2023-09-07 13:02:22 +0300

    React todo List

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,36 @@ import { useState } from 'react'
 
 // custom components
 import CustomForm from './components/CustomForm'
+import EditForm from './components/EditForm'
 import TaskList from './components/TaskList'
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [editedTask, setEditedTask] = useState([null]);
 
   const addTask = (task) => {
     setTasks(prevState => [ ... prevState, task])
+  }
+
+  const deleteTask = (id) => {
+    setTasks(prevState => prevState.filter(t => t.id !== id));
+  }
+
+  const toggleTask = (id) => {
+    setTasks(prevState => prevState.map(t => (
+      t.id == id
+        ? { ...t, checked: !t.checked }
+        : t
+      )))
+  }
+
+  const updateTask = (task) => {
+    setTasks(prevState => prevState.map(t => (
+      t.id == task.id
+        ? { ...t, name: task.name }
+        : t
+      )))
+      // close the edit mode
   }
 
   return (
@@ -16,8 +39,19 @@ function App() {
       <header>
         <h1>My Task List</h1>
       </header>
+      <EditForm 
+        editedTask={editedTask}
+        updateTask={updateTask}
+      />
       <CustomForm addTask={addTask}/>
-      {tasks && <TaskList tasks={task} />}
+      {tasks && (
+      <TaskList 
+        tasks={tasks}
+        deleteTask={deleteTask}
+        toggleTask={toggleTask}
+        enterEditMode={enterEditMode}
+        />
+      )}
     </div>
   )
 }
